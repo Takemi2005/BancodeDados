b157b87816672d03e4dcab20243b92867be2750f
Takemi2005 <takemiluiza321@gmail.com>
2025-08-01 16:43:52 -0300

    aulaBD

---
+++ Exemplo Script Modelo Escola.sql
@@ -0,0 +1,16 @@
+create database exemplo;
+
+use exemplo;
+
+-- cadastro aluno
+create table aluno (
+id_aluno int primary key
+);
+
+create table funcionario (
+id_funcionario int primary key 
+);
+
+create table curso (
+id_curso int primary key 
+);
